dff78b1d39b482bafd83412406858fa81d8419f6
Erin Van Brunt <erinmontybruce@gmail.com>
2022-04-10 17:23:25 -0700

    done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+# arr_D = ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves(arr)
+  if (arr != [])
+    arr.map.with_index do |character, idx|
+      puts "#{idx + 1}. #{character}"
+    end
+  else
+    puts "No dwarves in list."
+  end
 end
+# roll_call_dwarves(arr_D)
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+arr_P = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_planet(arr)
+  arr.map do |character|
+    puts "#{character[0].capitalize}#{character.slice(1, character.length - 1)}!"
+  end
 end
+# summon_captain_planet(arr_P)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def long_planeteer_calls(arr)
+  puts arr.any? {|word| word.length > 4}
 end
+# long_planeteer_calls(arr_P)
+
+# arr_C = ["mouse", "gouda", "lizard"]
+def find_the_cheese(arr)
+  cheeses = ["gouda", "cheddar", "camembert"]
+  arr.find do |maybe_cheese|
+   puts cheeses.include?(maybe_cheese)
+  end
+end
+# find_the_cheese(arr_C)
